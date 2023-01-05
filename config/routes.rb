5046f0df8ded737a4c51159ce68ff7186e20c6bc
mmccabe05 <mmccabe@chicagobooth.edu>
2023-01-05 02:48:44 +0000

    Updated as: methods for routes access

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create" 
-  get "/movies/new" => "movies#new" 
+  post "/movies" => "movies#create", as: :movies
+  get "/movies/new" => "movies#new", as: :new_movie 
           
   # READ
-  get "/movies" => "movies#index" 
-  get "/movies/:id" => "movies#show" 
+  get "/movies" => "movies#index"
+  get "/movies/:id" => "movies#show", as: :movie # movie_path() expects an argument
   
   # UPDATE
-  patch "/movies/:id" => "movies#update" 
-  get "/movies/:id/edit" => "movies#edit" 
+  patch "/movies/:id" => "movies#update"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie_path() expects an argument 
   
   # DELETE
   delete "/movies/:id" => "movies#destroy" 
